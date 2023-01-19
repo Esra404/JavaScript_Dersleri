0d9470a386c43bced3754c51bf4af682544b1b54
Esra404 <esradrmz571@gmail.com>
2023-01-19 03:14:38 +0300

    fix

---
+++ app1.js
@@ -0,0 +1,28 @@
+//Document write ile çıktı verme 
+// document.writeln("hello world <br>");
+// document.writeln("esra durmaz");
+// console.log("hello world");
+// console.log(true);
+// console.log(7.8);
+// console.log({name:"esra",surname:"derya"})
+// let a=5;
+// let b=6;
+// console.log("iki sayının toplamı=",a+b);
+// console.clear()
+// alert uyarı popup ile çıktı evrme işlemi
+// console.log(document.location.host);
+// console.log(window)
+// alert("merhaba")
+// let a=2;
+// let b=12;
+// alert("iki sayının toplamı="+(a+b));
+// alert("lütfen f5 tuşuna basmayınız");
+// console.log(window);
+// console.log(window.location.port);
+console.warn("hata var");
+debugger;
+console.log(location.port);
+/*
+
+? bil
+*/
---
+++ app2.js
@@ -0,0 +1,37 @@
+// var-let-cons
+/*
+----------------SCOPE(KAPSAM)------------------
+-->GLOBAL SCOPE ------ HER YERDEN ERİŞİLEBİLİR
+-->FUNCTİON SCOPE ------- fonksiyon içinde olacak
+-->BLOCK SCOPE------------ bloklar içinde if else gibi
+
+*/
+// var a=4;
+// if(true){
+
+// }
+// function method1(){
+//     console.log(a);
+// }
+// method1(1); 
+// var b=665;
+// function method1(){
+//     var sayi=12;
+//     console.log(b);
+//     // console.sayi(sayi);
+// }
+// method1();
+
+// function esra(){
+//     var a=5; function
+//     if(true){
+//         var b=78;  block
+//     }
+//     while(true){
+
+//     }
+//     for(let i=0;i<=89;i++){
+
+//     }
+
+// }
