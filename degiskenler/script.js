cd0953d5c959172b8f8ecc21026af465776bc230
Esra404 <esradrmz571@gmail.com>
2023-04-18 11:51:44 +0300

    değişkenler

---
+++ degiskenler/script.js
@@ -0,0 +1,1 @@
+console.log("sayfa yüklendi");
---
+++ degiskenler/script1.js
@@ -0,0 +1,9 @@
+
+var ad=prompt("adınızı giirniz:" );
+var ogrno=prompt("ogrenci no");
+var cimsiyet=promt("cinsiyet");
+var ders=promt("ders adı 1=");
+var not1=prompt("not 1=");
+var not2=promt("ders2=");
+var ortalama=(not1+not2)/2;
+console.log(ogrno+"numaralı "+ad+"("+cinsiyet+")"+"isimli öğrencinin "+ders +"dersinden aldığı ortalama"+ortalama);
