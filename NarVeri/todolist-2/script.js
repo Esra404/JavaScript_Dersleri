feb42eb882b3cb84bfb6d8ef4c7aa6b83f3cd132
Esra404 <esradrmz571@gmail.com>
2023-06-12 11:45:35 +0300

    to-do list

---
+++ NarVeri/todolist-2/script.js
@@ -0,0 +1,12 @@
+var todos = [];
+
+document.getElementById('button').addEventListener('click', function 
+addTodo () {
+todos.push('input')  
+
+function printTodos () {
+    var item = document.createElement("li");
+    var node = createTextNode(input);
+    // I am stuck 
+}
+});
