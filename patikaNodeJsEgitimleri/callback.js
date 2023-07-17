8377612494d2de84e8ee8783b566b890b8162172
Esra404 <esradrmz571@gmail.com>
2023-07-17 03:03:48 +0300

    learn callback function

--- patikaNodeJsEgitimleri/callback.js
+++ patikaNodeJsEgitimleri/callback.js
@@ -1,19 +1,55 @@
-const books = [
-	{ name: "Kitap 1", author: "Yazar 1" },
-	{ name: "Kitap 2", author: "Yazar 2" },
-	{ name: "Kitap 3", author: "Yazar 3" },
-          ];
-          
-          const listBooks = () => {
-	books.map((book) => {
-	  console.log(book.name);
-	});
-          };
-          
-          const addBook = (newBook, callback) => {
+/*const func1=() =>{
+	console.log("func 1 tamamlandı");
+};
+
+const func2=()=>{
+	console.log("func 2 tamamlandı");
+};
+
+func2();
+func1();
+
+const func1=() =>{
+	console.log("func 1 tamamlandı");
+	func3();
+};
+
+const func2=()=>{
+	console.log("func 2 tamamlandı");
+};
+const func3=()=>{
+	console.log("func 3 tamamlandı");
+	func2();
+};
+
+func1();
+
+let x=5;
+console.log(x);
+
+setTimeout(()=>{
+	x=x+5;
+},5000);
+
+console.log(x);
+
+x=x+5;
+console.log(x);
+*/
+const books=[
+	{name:'kitap 1',author:'yazar 1'},
+	{name:'kitap 2',author:'yazar 2'},
+	{name:'kitap 3',author:'yazar 3'}
+];
+const listBooks=()=>{
+	books.map(book=>{
+		console.log(book.name);
+	})
+};
+
+const addBook=(newBook,callback)=>{
 	books.push(newBook);
 	callback();
-          };
-          
-          addBook({ name: "Kitap 4", author: "Yazar 4" }, listBooks);
-          
+};
+addBook({name:'Kitap4',author:'yazar4'},listBooks);
+//listBooks();
